99c5cc5f232154eb5650c377ac1c6e24406d30b2
Graham Rowlands <grahamrow@gmail.com>
2017-02-13 15:17:26 -0500

    Moving pulse cal helper functions to beginning of file

--- src/auspex/pulse_calibration.py
+++ src/auspex/pulse_calibration.py
@@ -31,6 +31,84 @@ def calibrate(calibrations):
             raise TypeError("calibrate_pulses was passed a calibration that is not actually a calibration.")
         calibration.calibrate()
 
+def restrict(phase):
+    out = np.mod( phase + np.pi, 2*np.pi, ) - np.pi
+    return out
+
+def phase_estimation( data_in, vardata_in, verbose=False):
+    """Estimates pulse rotation angle from a sequence of P^k experiments, where
+    k is of the form 2^n. Uses the modified phase estimation algorithm from
+    Kimmel et al, quant-ph/1502.02677 (2015). Every experiment i doubled.
+    vardata should be the variance of the mean"""
+
+    #average together pairs of data points
+    avgdata = (data_in[0::2] + data_in[1::2])/2
+
+    # normalize data using the first two pulses to calibrate the "meter"
+    data = 1 + 2*(avgdata[2:] - avgdata[0]) / (avgdata[0] - avgdata[1])
+    zdata = data[0::2]
+    xdata = data[1::2]
+
+    # similar scaling with variances
+    vardata = (vardata_in[0::2] + vardata_in[1::2])/2
+    vardata = vardata[2:] * 2 / abs(avgdata[0] - avgdata[1])**2
+    zvar = vardata[0::2]
+    xvar = vardata[1::2]
+
+    phases = np.arctan2(xdata, zdata)
+    distances = np.sqrt(xdata**2 + zdata**2)
+
+    curGuess = phases[0]
+    phase = curGuess
+    sigma = np.pi
+
+    if verbose == True:
+        print('Current Guess: %f'%(curGuess))
+
+    for k in range(1,len(phases)):
+
+        if verbose == True:
+            print('k: %d'%(k))
+
+        # Each step of phase estimation needs to assign the measured phase to
+        # the correct half circle. We will conservatively require that the
+        # (x,z) tuple is long enough that we can assign it to the correct
+        # quadrant of the circle with 2σ confidence
+
+        if distances[k] < 2*np.sqrt(xvar[k] + zvar[k]):
+            print('Phase estimation terminated at %dth pulse because the (x,z) vector is too short'%(k))
+            break
+
+        lowerBound = restrict(curGuess - np.pi/2**(k))
+        upperBound = restrict(curGuess + np.pi/2**(k))
+        possiblesTest = [ restrict((phases[k] + 2*n*np.pi)/2**(k)) for n in range(0,2**(k)+1)]
+
+        if verbose == True:
+            print('Lower Bound: %f'%lowerBound)
+            print('Upper Bound: %f'%upperBound)
+
+        possibles=[]
+        for p in possiblesTest:
+            # NOTE: previous code did not handle upperbound == lowerBound
+            if lowerBound >= upperBound:
+                satisfiesLB = p > lowerBound or p < 0.
+                satisfiesUP = p < upperBound or p > 0.
+            else:
+                satisfiesLB = p > lowerBound
+                satisfiesUP = p < upperBound
+
+            if satisfiesLB == True and satisfiesUP == True:
+                possibles.append(p)
+
+        curGuess = possibles[0]
+        if verbose == True:
+            print('Current Guess: %f'%(curGuess))
+
+        phase = curGuess
+        sigma = np.maximum(np.abs(restrict(curGuess - lowerBound)), np.abs(restrict(curGuess - upperBound)))
+
+    return phase, sigma
+
 class PulseCalibration(object):
     """Base class for calibration of qubit control pulses."""
     def __init__(self, qubit_name, notebook=True):
@@ -304,81 +382,3 @@ class DRAGCalibration(PulseCalibration):
         self.update_libraries([chan_settings], [config.channelLibFile])
 
         return fitted_drag
-
-def restrict(phase):
-    out = np.mod( phase + np.pi, 2*np.pi, ) - np.pi
-    return out
-
-def phase_estimation( data_in, vardata_in, verbose=False):
-    """Estimates pulse rotation angle from a sequence of P^k experiments, where
-    k is of the form 2^n. Uses the modified phase estimation algorithm from
-    Kimmel et al, quant-ph/1502.02677 (2015). Every experiment i doubled.
-    vardata should be the variance of the mean"""
-
-    #average together pairs of data points
-    avgdata = (data_in[0::2] + data_in[1::2])/2
-
-    # normalize data using the first two pulses to calibrate the "meter"
-    data = 1 + 2*(avgdata[2:] - avgdata[0]) / (avgdata[0] - avgdata[1])
-    zdata = data[0::2]
-    xdata = data[1::2]
-
-    # similar scaling with variances
-    vardata = (vardata_in[0::2] + vardata_in[1::2])/2
-    vardata = vardata[2:] * 2 / abs(avgdata[0] - avgdata[1])**2
-    zvar = vardata[0::2]
-    xvar = vardata[1::2]
-
-    phases = np.arctan2(xdata, zdata)
-    distances = np.sqrt(xdata**2 + zdata**2)
-
-    curGuess = phases[0]
-    phase = curGuess
-    sigma = np.pi
-
-    if verbose == True:
-        print('Current Guess: %f'%(curGuess))
-
-    for k in range(1,len(phases)):
-
-        if verbose == True:
-            print('k: %d'%(k))
-
-        # Each step of phase estimation needs to assign the measured phase to
-        # the correct half circle. We will conservatively require that the
-        # (x,z) tuple is long enough that we can assign it to the correct
-        # quadrant of the circle with 2σ confidence
-
-        if distances[k] < 2*np.sqrt(xvar[k] + zvar[k]):
-            print('Phase estimation terminated at %dth pulse because the (x,z) vector is too short'%(k))
-            break
-
-        lowerBound = restrict(curGuess - np.pi/2**(k))
-        upperBound = restrict(curGuess + np.pi/2**(k))
-        possiblesTest = [ restrict((phases[k] + 2*n*np.pi)/2**(k)) for n in range(0,2**(k)+1)]
-
-        if verbose == True:
-            print('Lower Bound: %f'%lowerBound)
-            print('Upper Bound: %f'%upperBound)
-
-        possibles=[]
-        for p in possiblesTest:
-            # NOTE: previous code did not handle upperbound == lowerBound
-            if lowerBound >= upperBound:
-                satisfiesLB = p > lowerBound or p < 0.
-                satisfiesUP = p < upperBound or p > 0.
-            else:
-                satisfiesLB = p > lowerBound
-                satisfiesUP = p < upperBound
-
-            if satisfiesLB == True and satisfiesUP == True:
-                possibles.append(p)
-
-        curGuess = possibles[0]
-        if verbose == True:
-            print('Current Guess: %f'%(curGuess))
-
-        phase = curGuess
-        sigma = np.maximum(np.abs(restrict(curGuess - lowerBound)), np.abs(restrict(curGuess - upperBound)))
-
-    return phase, sigma
